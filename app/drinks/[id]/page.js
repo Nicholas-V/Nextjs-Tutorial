da4eea59e7214015b2a51ef0c23425af0c44731c
Nicholas-V <MykolaVasylyniuk@gmail.com>
2024-07-13 12:48:38 -0400

    init

---
+++ app/drinks/[id]/page.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import drinkImg from './drink.jpg';
+import Image from "next/image";
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+console.log(drinkImg);
+
+const getSingleDrink = async (id) => {
+    const res = await fetch(`${url}${id}`)
+    if (!res.ok) {
+        throw new Error('Failed to fetch a drink...')
+    }
+    return res.json();
+}
+
+const SingleDrinkPage = async ({params}) => {
+    const data = await getSingleDrink(params.id);
+    const title = data?.drinks[0]?.strDrink;
+    const imgSrc = data?.drinks[0]?.strDrinkThumb;
+    console.log(title, imgSrc);
+    return (
+        <div>
+            <Link href="/drinks" className="btn btn-primary mt-8 mb-12 capitalize">
+                back to drinks
+            </Link>
+            {/*<Image src={drinkImg} className="w-48 h-48 rounded" alt="drink" />*/}
+            <Image src={imgSrc} width={300} height={300} className="w-48 h-48 rounded-lg shadow-lg mb-4" priority
+                   alt={title}/>
+            <h1 className="text-4xl mb-8">{title}</h1>
+        </div>
+    )
+}
+export default SingleDrinkPage;
